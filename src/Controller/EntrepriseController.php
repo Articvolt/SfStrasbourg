178ac263e5b052ca65f8feed4f81138e62ccde6c
Articvolt <ugodasilva67@gmail.com>
2022-11-21 11:33:06 +0100

    show list of entreprise in template/entreprise/index

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EntrepriseController extends AbstractController
 {
     /**
      * @Route("/entreprise", name="app_entreprise")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // test en ajoutant un tableau
+        // $tableau = ["valeur 1","valeur 2","valeur 3","valeur 4","valeur 5"];
+
+        // fonction qui récupère les entreprises dans la BDO
+        // https://symfony.com/doc/current/doctrine.html#fetching-objects-from-the-database
+        $entreprises = $doctrine->getRepository(Entreprise::class)->findAll();
         return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+            'entreprises' => $entreprises
+
+            // test en ajoutant à name, une valeur (ici -> Mickael)
+            // 'name'=>'Mickael',
+            // 'tableau' => $tableau
         ]);
     }
 }
